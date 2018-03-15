dae2ec6b345491f8152962b4359a6ba76d3ff52f
Your Name <mondals@g.ucla.edu>
2018-03-15 01:07:15 +0000

    adding to the hw

--- hw4/hw4.R
+++ hw4/hw4.R
@@ -1,6 +1,12 @@
 library(sparklyr)
 library(ggplot2)
+library(dplyr)
 Sys.setenv(SPARK_HOME="/usr/lib/spark")
 config <- spark_config()
 sc <- spark_connect(master = "yarn-client", config = config)
 sc
+
+flights_tbl <- tbl(sc, 'flights')
+airlines_tbl <- tbl(sc, 'airlines')
+airports_tbl <- tbl(sc, 'airports')
+
